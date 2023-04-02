8cf0e650dfe557052c7255705704b6d7ac181e18
Logan Cox <119902648+coxlogan209@users.noreply.github.com>
2023-04-02 08:46:13 -0400

    Add files via upload

---
+++ M02Lab.py
@@ -0,0 +1,13 @@
+#Logan Cox M02Lab.py This app requires a student's first name, last name, and gpa. It will then determine if they are on the Dean's list or honor roll.
+
+
+while True:
+    last_name = input("Enter student's last name (or ZZZ to quit): ")
+    if last_name == 'ZZZ':
+        break
+    first_name = input("Enter student's first name: ")
+    gpa = float(input("Enter student's GPA: "))
+    if gpa >= 3.5:
+        print(f"{first_name} {last_name} made the Dean's List!")
+    elif gpa >= 3.25:
+        print(f"{first_name} {last_name} made the Honor Roll!")
